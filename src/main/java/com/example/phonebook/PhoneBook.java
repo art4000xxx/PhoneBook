94a7dd3d9d70c57d264933140607bfff9cee0cd9
Artem <art4000@list.ru>
2025-04-28 09:30:55 +0600

    "add implemented"

--- src/main/java/com/example/phonebook/PhoneBook.java
+++ src/main/java/com/example/phonebook/PhoneBook.java
@@ -1,7 +1,15 @@
 package com.example.phonebook;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+    private final Map<String, String> contacts = new HashMap<>();
+
     public int add(String name, String number) {
-        return 0;
+        if (!contacts.containsKey(name)) {
+            contacts.put(name, number);
+        }
+        return contacts.size();
     }
 }
